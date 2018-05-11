b9d579ae92312c284f46f1bb6d2a6be91fc4350b
Mark Moser <MarkAMoser@gmail.com>
2018-05-11 08:33:08 -0500

    close modal in ESC keyup
    closes #7

--- src/js/main/modal.js
+++ src/js/main/modal.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
   var overlay = document.querySelector(".modal-overlay");
 
+  // close modal on overlay click
   overlay.addEventListener('click', function(){
     this.classList.remove('active');
 
@@ -10,6 +11,19 @@ document.addEventListener("DOMContentLoaded", function(){
     });
   });
 
+  // close open modal on ESC
+  document.addEventListener('keyup', function(event){
+    if (event.defaultPrevented){ return; }
+    if (document.querySelector('.pin-modal.active') == null){ return; }
+
+    var key = event.key || event.keyCode;
+    if (key === 'Escape' || key === 'Esc' || key === 27) {
+      document.querySelector('.pin-modal.active').classList.remove('active');
+      overlay.classList.remove('active');
+    }
+  });
+
+  // close modal on dismiss click
   document.querySelectorAll('.pin-modal-dismiss').forEach(function(dismiss){
     dismiss.addEventListener('click', function(){
       this.closest('.pin-modal.active').classList.remove('active');
@@ -17,6 +31,7 @@ document.addEventListener("DOMContentLoaded", function(){
     });
   });
 
+  // open modal on toggler click
   document.querySelectorAll('.pin-modal-toggle').forEach(function(toggle){
     toggle.addEventListener('click', function(){
       var modal = document.querySelector("#"+toggle.getAttribute('data-modal'));
@@ -25,6 +40,7 @@ document.addEventListener("DOMContentLoaded", function(){
     });
   });
 
+  // swap viewer images
   document.querySelectorAll('.pin-modal-thumb').forEach(function(thumb){
     thumb.addEventListener('click', function(){
       var viewer = thumb.closest(".pin-modal-container ").querySelector(".pin-modal-viewer");
