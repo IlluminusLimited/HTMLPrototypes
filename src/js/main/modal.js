29354f31830a5ac43fb3f6be1a9c6a611cae6a42
Mark Moser <MarkAMoser@gmail.com>
2018-05-11 08:57:06 -0500

    prevent page scrolling behind open modal
    closes #3

--- src/js/main/modal.js
+++ src/js/main/modal.js
@@ -1,42 +1,46 @@
+function openModal(toggle){
+  var modal = document.querySelector("#"+toggle.getAttribute('data-modal'));
+  modal.classList.add("active");
+  document.querySelector(".modal-overlay").classList.add("active");
+  document.querySelector('html').classList.add('no-scroll');
+}
+
+function closeModal(){
+  document.querySelector('html').classList.remove('no-scroll');
+  document.querySelector(".modal-overlay").classList.remove("active");
+  document.querySelectorAll('.pin-modal.active').forEach(function(modal){
+    modal.classList.remove('active');
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function(){
 
-  var overlay = document.querySelector(".modal-overlay");
-
   // close modal on overlay click
-  overlay.addEventListener('click', function(){
-    this.classList.remove('active');
-
-    document.querySelectorAll('.pin-modal.active').forEach(function(modal){
-      modal.classList.remove('active');
-    });
+  document.querySelector(".modal-overlay").addEventListener('click', function(){
+    closeModal();
   });
 
   // close open modal on ESC
   document.addEventListener('keyup', function(event){
     if (event.defaultPrevented){ return; }
-    if (document.querySelector('.pin-modal.active') == null){ return; }
 
     var key = event.key || event.keyCode;
     if (key === 'Escape' || key === 'Esc' || key === 27) {
-      document.querySelector('.pin-modal.active').classList.remove('active');
-      overlay.classList.remove('active');
+      closeModal();
     }
   });
 
   // close modal on dismiss click
   document.querySelectorAll('.pin-modal-dismiss').forEach(function(dismiss){
     dismiss.addEventListener('click', function(){
-      this.closest('.pin-modal.active').classList.remove('active');
-      overlay.classList.remove('active');
+      closeModal();
     });
   });
 
   // open modal on toggler click
   document.querySelectorAll('.pin-modal-toggle').forEach(function(toggle){
     toggle.addEventListener('click', function(){
-      var modal = document.querySelector("#"+toggle.getAttribute('data-modal'));
-      overlay.classList.add("active");
-      modal.classList.add("active");
+      openModal(toggle);
     });
   });
 
